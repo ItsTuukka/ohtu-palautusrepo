8f06509869c6cd8c8f37dcecdcb34e709addc0e5
ItsTuukka <tuukka.ylostalo@helsinki.fi>
2022-11-28 21:37:49 +0200

    Metodi tyhjenna tyhjentää korin

--- viikko4/tdd-ostoskori/src/ostoskori.py
+++ viikko4/tdd-ostoskori/src/ostoskori.py
@@ -42,7 +42,7 @@ class Ostoskori:
                     return
 
     def tyhjenna(self):
-        pass
+        self.kori = []
         # tyhjentää ostoskorin
 
     def ostokset(self):
--- viikko4/tdd-ostoskori/src/tests/ostoskori_test.py
+++ viikko4/tdd-ostoskori/src/tests/ostoskori_test.py
@@ -105,3 +105,13 @@ class TestOstoskori(unittest.TestCase):
         self.assertEqual(len(ostokset), 0)
         self.assertEqual(self.kori.tavaroita_korissa(), 0)
         self.assertEqual(self.kori.hinta(), 0)
+    
+    def test_metodi_tyhjenna_tyhjentaa_korin(self):
+        maito = Tuote("Maito", 3)
+        self.kori.lisaa_tuote(maito)
+        self.kori.tyhjenna()
+
+        ostokset = self.kori.ostokset()
+        self.assertEqual(len(ostokset), 0)
+        self.assertEqual(self.kori.tavaroita_korissa(), 0)
+        self.assertEqual(self.kori.hinta(), 0)
